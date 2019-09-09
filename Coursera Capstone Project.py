f462363df3a27344921555686b2ae65f1a2d385b
ASkorupa1990 <55066080+ASkorupa1990@users.noreply.github.com>
2019-09-09 10:39:38 +0200

    Add files via upload

---
+++ Coursera Capstone Project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is a assingment for the Capstone Project
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project")
+
